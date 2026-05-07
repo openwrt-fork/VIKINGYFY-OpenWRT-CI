569a0a84e3b712f84a72e3da646818e6f633acb5
luomo <luomoxu@fake.com>
2026-05-07 09:44:17 +0800

    优化构建

--- Scripts/PRIVATE.sh
+++ Scripts/PRIVATE.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 UPDATE_PACKAGE "nps-openwrt" "djylb/nps-openwrt" "main" "" "npc nps luci-app-npc luci-app-nps"
+UPDATE_PACKAGE "passwall-packages" "Openwrt-Passwall/openwrt-passwall-packages" "main" "" "xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls"
